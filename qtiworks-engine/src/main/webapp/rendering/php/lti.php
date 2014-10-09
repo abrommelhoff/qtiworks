d16fefbcaf84f2e80efaea0ac52bdb4396d2c718
ndechman <ndechman@vortwebsys.com>
2014-10-09 10:52:45 -0400

    trying another way to get back to coordinator

--- qtiworks-engine/src/main/webapp/rendering/php/lti.php
+++ qtiworks-engine/src/main/webapp/rendering/php/lti.php
@@ -36,7 +36,7 @@ session_start();
     "lis_person_name_given" => 'Given',
     "lis_person_contact_email_primary" => "user@school.edu",
     "lis_person_sourcedid" => "school.edu:user",
-    "lis_outcome_service_url" => "http://innoca01-nc-dv.measuredprogress.org/coordinator/Student/Result.aspx", 
+//    "lis_outcome_service_url" => "http://innoca01-nc-dv.measuredprogress.org/coordinator/Student/Result.aspx", 
     "context_id" => "456434513",
     "context_title" => "Design of Personal Environments",
     "context_label" => "SI182",
@@ -128,7 +128,7 @@ function lmsdataToggle() {
     $parms["lis_result_sourcedid"] = "feb-123-456-2929::28883";
   }
 
-  $parms['launch_presentation_return_url'] = $returl;
+  $parms['launch_presentation_return_url'] = "http://innoca01-nc-dv.measuredprogress.org/coordinator/Student/Result.aspx";
 
   $custom = explode("\n", $custom);
   foreach ($custom as $line) {
