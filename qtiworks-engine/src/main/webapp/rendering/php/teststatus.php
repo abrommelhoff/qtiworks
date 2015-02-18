5f5601a267bde9090e1e3bfe29688fafbf6bf470
ndechman <ndechman@vortwebsys.com>
2015-02-17 20:47:57 -0500

    test status php

--- qtiworks-engine/src/main/webapp/rendering/php/teststatus.php
+++ qtiworks-engine/src/main/webapp/rendering/php/teststatus.php
@@ -4,7 +4,7 @@ function getStatus($dbname)
 {
 	include 'DBInfo.php';
 	mysql_connect($dbHost,$dbUser,$dbPass) or die("no connect");
-	mysql_select_db($dbname);
+	mysql_select_db($dbname); 
 	$result = mysql_query("select candidate_events.timestamp, candidate_events.test_event_type, deliveries.lti_consumer_key_token, deliveries.lti_consumer_secret from lti_users join candidate_sessions on candidate_sessions.candidate_uid=lti_users.id join candidate_events on candidate_events.xid=candidate_sessions.xid left outer join candidate_session_outcomes on candidate_session_outcomes.xid=candidate_sessions.xid join deliveries on deliveries.did=candidate_sessions.did where lti_user_id".$_REQUEST["id"]);
 	if(mysql_num_rows($result) == 0) { 
 		return false;
