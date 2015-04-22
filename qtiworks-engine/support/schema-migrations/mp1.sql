921c89493f99f077d95936b5752511753082b344
ndechman <ndechman@vortwebsys.com>
2015-04-22 10:31:38 -0400

    removing unnecessary sql statements

--- qtiworks-engine/support/schema-migrations/mp1.sql
+++
@@ -1,5 +0,0 @@
---alter table candidate_responses add response_correctness varchar(10);
---alter table candidate_responses add time_on_task varchar(10);
---alter table candidate_responses add response_feedback longtext;
---alter table candidate_responses add misconception_type longtext;
---alter table candidate_responses add misconception_value varchar(16);
