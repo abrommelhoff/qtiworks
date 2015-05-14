b2ab09809736229db56c05b35e7da0ce51d3476e
ndechman <ndechman@vortwebsys.com>
2015-05-13 22:38:59 -0400

    an interesting thing to try

--- qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/running/TestSessionController.java
+++ qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/running/TestSessionController.java
@@ -1556,12 +1556,13 @@ public final class TestSessionController extends TestProcessingController {
         final TestPlanNode currentItemRefNode = expectItemRefNode(currentItemKey);
         final EffectiveItemSessionControl effectiveItemSessionControl = currentItemRefNode.getEffectiveItemSessionControl();
         if (!effectiveItemSessionControl.isAllowComment()) {
-            throw new QtiCandidateStateException("The item has allowComment=false, so setting a candidate comment is not permitted");
-        }
-        logger.debug("Setting candidate comment to {}", candidateComment);
+            //throw new QtiCandidateStateException("The item has allowComment=false, so setting a candidate comment is not permitted");
+        } else {
+        	logger.debug("Setting candidate comment to {}", candidateComment);
 
-        final ItemSessionController itemSessionController = getItemSessionController(currentItemRefNode);
-        itemSessionController.setCandidateComment(timestamp, candidateComment);
+        	final ItemSessionController itemSessionController = getItemSessionController(currentItemRefNode);
+        	itemSessionController.setCandidateComment(timestamp, candidateComment);
+        }
     }
 
     //-------------------------------------------------------------------
